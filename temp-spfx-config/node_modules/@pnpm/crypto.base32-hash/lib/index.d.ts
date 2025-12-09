5ff582c540dcdc8cc1fa7bcc8d55a28cdaeb7858
Vinod M <vinodkumar@rigaps.com>
2025-12-09 13:01:06 +0530

    Commit 4

--- temp-spfx-config/node_modules/@pnpm/crypto.base32-hash/lib/index.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function createBase32Hash(str: string): string;
-export declare function createBase32HashFromFile(file: string): Promise<string>;
--- temp-spfx-config/node_modules/@rushstack/localization-utilities/README.md
+++
@@ -1,3 +0,0 @@
-# @rushstack/localization-utilities
-
-This library provides a set of utilities for working with localization files.
